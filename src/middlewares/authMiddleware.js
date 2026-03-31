de9ae7dd789392a4b1b039fb149fb836e1601348
xuanduc2705 <bxd27052003@gmail.com>
2026-03-31 14:52:34 +0700

    up

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -14,15 +14,10 @@ export const authMiddleware = async (req, res, next) => {
   if (!auth) return errorResponse(res, 401, '로그인되어 있지 않습니다.', 'UNAUTHORIZED');
   const token = auth.replace('Bearer ', '');
   try {
-    console.log('Token:', token);
-
     const payload = verifyAccessToken(token);
-    console.log('Payload:', payload);
-
     const user_id = payload.sub;
     const account = await detailUserDemoMd({ id: user_id });
     if (!account) return errorResponse(res, 401, '해당 계정은 존재하지 않습니다.', 'ACCOUNT_NOT_FOUND');
-    if (account.status !== 1) return errorResponse(res, 403, '계정이 잠겼습니다.', 'ACCOUNT_DISABLED');
     req.userInfo = { ...account };
     next();
   } catch (err) {
